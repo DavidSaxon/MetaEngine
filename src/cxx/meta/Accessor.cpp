347ac44afff0f8fbd2d045a62da748253f343255
David Saxon <davidesaxon@gmail.com>
2016-03-28 22:20:45 +1300

    experimenting with accessors

--- src/cxx/meta/Accessor.cpp
+++ src/cxx/meta/Accessor.cpp
@@ -1,5 +1,10 @@
 #include "meta/Accessor.hpp"
 
+#include <chaoscore/io/sys/FileSystemOperations.hpp>
+
+// TODO: REMOVE ME
+#include <iostream>
+
 namespace meta
 {
 
@@ -9,8 +14,18 @@ namespace meta
 
 Accessor::Accessor(const chaos::io::sys::Path& path)
     :
-    m_path(path)
+    m_path        (path),
+    m_is_directory(chaos::io::sys::is_directory(path))
 {
+    // initialise based on the type
+    if (m_is_directory)
+    {
+        directory_init();
+    }
+    else
+    {
+        file_init();
+    }
 }
 
 //------------------------------------------------------------------------------
@@ -20,6 +35,48 @@ Accessor::Accessor(const chaos::io::sys::Path& path)
 const chaos::io::sys::Path& Accessor::get_path() const
 {
     return m_path;
+}
+
+bool Accessor::is_directory() const
+{
+    return m_is_directory;
+}
+
+const std::vector<chaos::uni::UTF8String>& Accessor::get_components() const
+{
+    return m_components;
+}
+
+//------------------------------------------------------------------------------
+//                            PRIVATE MEMBER FUNCTIONS
+//------------------------------------------------------------------------------
+
+void Accessor::directory_init()
+{
+    // get the files under this directory that end with .meta
+    std::vector<chaos::io::sys::Path> paths(chaos::io::sys::list(m_path));
+    CHAOS_FOR_EACH(it, paths)
+    {
+        // make sure this isn't . or ..
+        if (it->get_back() == "." ||
+            it->get_back() == ".."   )
+        {
+            continue;
+        }
+
+        // if this is a file check that it ends with .meta
+        if (chaos::io::sys::is_file(m_path) && m_path.get_extension() != "meta")
+        {
+            continue;
+        }
+
+        m_components.push_back(it->get_back());
+    }
+}
+
+void Accessor::file_init()
+{
+    // TODO: open config file and begin reading JSON
 }
 
 } // namespace meta
--- src/cxx/meta/Accessor.hpp
+++ src/cxx/meta/Accessor.hpp
@@ -39,6 +39,19 @@ public:
      */
     const chaos::io::sys::Path& get_path() const;
 
+    /*!
+     * \brief Returns whether this Accessor points to a directory or an actual
+     *        data file.
+     *
+     * \returns True if this Accessor points to a directory.
+     */
+    bool is_directory() const;
+
+    /*!
+     * \brief Return the names of the components under this accessors.
+     */
+    const std::vector<chaos::uni::UTF8String>& get_components() const;
+
 private:
 
     //--------------------------------------------------------------------------
@@ -48,7 +61,31 @@ private:
     /*!
      * \brief The file or directory path this Accessor is reading.
      */
-    chaos::io::sys::Path m_path;
+    const chaos::io::sys::Path m_path;
+    /*!
+     * \brief Stores whether this Accessor points to a directory or not at the
+     *        time it was created.
+     */
+    const bool m_is_directory;
+
+    /*!
+     * \brief The list of components under this Accessors.
+     */
+    std::vector<chaos::uni::UTF8String> m_components;
+
+    //--------------------------------------------------------------------------
+    //                          PRIVATE MEMBER FUNCTIONS
+    //--------------------------------------------------------------------------
+
+    /*!
+     * \brief Initialises this Accessor as a directory.
+     */
+    void directory_init();
+
+    /*!
+     * \breief Initialises this Accessor as a configuration file.
+     */
+    void file_init();
 };
 
 } // namespace meta
