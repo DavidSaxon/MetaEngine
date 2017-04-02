7ef6c239aa78d8bbef315253e2142a49f805de98
David Saxon <davidesaxon@gmail.com>
2017-04-03 00:29:58 +1200

    updated for windows 10

--- tools/w_build.bat
+++ tools/w_build.bat
@@ -1,4 +1,2 @@
-set VCTargetsPath=c:\Program Files (x86)\MSBuild\Microsoft.Cpp\v4.0\V120
-
 msbuild ..\MetaEngine.sln /v:n /p:Configuration=Lib
 msbuild ..\MetaEngine.sln /v:n /p:Configuration=tests
